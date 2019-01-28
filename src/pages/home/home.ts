0c6083d30f6436a72ee6511f75c2903f1e5ecc0a
David <danaya@gmail.com>
2019-01-28 18:39:58 +0100

    feat: use forked facebook

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+
+declare var cordova: any;
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  checked = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  toggle() {
+    this.checked = !this.checked;
+
+    if (cordova) {
+      cordova.plugins["cordova-plugin-facebook4"].setAutoLogAppEventsEnabled(
+        this.checked,
+        this.success,
+        this.failure
+      );
+    }
   }
 
+  private success(result) {
+    const captain = console;
+    captain.log("success setAutoLogAppEventsEnabled", result);
+  }
+
+  private failure(result) {
+    const captain = console;
+    captain.log("failure setAutoLogAppEventsEnabled", result);
+  }
 }
